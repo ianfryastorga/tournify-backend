f41b2f71d344b90fb64eed72393579ae9b6bc85e
XXiaoR <xli@uc.cl>
2024-10-16 22:11:13 -0300

    FEAT: add endpoints for 'Usuarios' and 'Torneos'

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ yarn start
 ### Para generar migraciones en la bdd:
 yarn sequelize-cli db:migrate
 
-### Para agregar seeds (de torneo) a la bdd:
+### Para agregar seeds a la bdd:
 yarn sequelize-cli db:seed:all
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const Router = require("koa-router");
 const authentication = require("./routes/authentication");
+const usuarios = require("./routes/usuarios");
+const torneos = require("./routes/torneos");
 
 const router = new Router();
 
 router.use("/auth", authentication.routes());
+router.use("/usuarios", usuarios.routes());
+router.use("/torneos", torneos.routes());   
 
 module.exports = router;
