5505b58ee52320b4b524e3fa10d5c7085e35496d
Paul Mac Guire Macchiavello <101202383+paulmacguire@users.noreply.github.com>
2024-11-06 11:36:08 -0300

    Revert "feat: hash passwords and update user roles in seed data"

--- src/seeders/20241016200002-seed-users.js
+++ src/seeders/20241016200002-seed-users.js
@@ -1,48 +1,41 @@
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
-
 module.exports = {
-  up: async (queryInterface) => {
-    const hashedPassword = bcrypt.hashSync("Hola123%", saltRounds);
-
-    await queryInterface.bulkInsert('Users', [
-      {
-        name: 'Tomás González',
-        email: 'tgonza@uc.cl',
-        password: hashedPassword,
-        gender: 'Masculino',
-        role: 'Capitan',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: 'María Pérez',
-        email: 'mperez@uc.cl',
-        password: hashedPassword,
-        gender: 'Femenino',
-        role: 'Jugador',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Martini Ramirez',
-        email: 'mabe@uc.cl',
-        password: hashedPassword,
-        gender: 'Femenino',
-        role: 'Administrador',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Juaquín Paiva',
-        email: 'paiva@uc.cl',
-        password: hashedPassword,
-        gender: 'Masculino',
-        role: 'Jugador',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
-  },
+  up: (queryInterface) => queryInterface.bulkInsert('Users', [
+    {
+      name: 'Tomás González',
+      email: 'tgonza@uc.cl',
+      password: '1234',
+      gender: 'Masculino',
+      role: 'Organizador',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'María Pérez',
+      email: 'mperez@uc.cl',
+      password: '1234',
+      gender: 'Femenino',
+      role: 'Jugador',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Martini Ramirez',
+      email: 'mabe@uc.cl',
+      password: '1234',
+      gender: 'Femenino',
+      role: 'Administrador',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      name: 'Juaquín Paiva',
+      email: 'paiva@uc.cl',
+      password: '1234',
+      gender: 'Masculino',
+      role: 'Jugador',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]),
   down: (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
-};
+}
